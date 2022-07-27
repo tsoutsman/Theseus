a7639f3f68f3ec370974ad7c897821d81a9c4e96
Klim Tsoutsman <klim@tsoutsman.com>
2022-07-27 19:18:47 +1000

    Version 2
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- stub/src/lib.rs
+++ stub/src/lib.rs
@@ -1,6 +1,11 @@
 #![no_std]
 
-#[export_name = "terminal_print::print_to_stdout_args::h2e6f0d1c9e7a336c"]
-#[no_mangle]
-#[inline(never)]
-pub fn print_to_stdout_args(fmt_args: core::fmt::Arguments) {}
+extern "Rust" {
+    #[link_name = "terminal_print::print_to_stdout_args::h2e6f0d1c9e7a336c"]
+    pub fn _print_to_stdout_args(fmt_args: core::fmt::Arguments);
+}
+
+pub fn print_to_stdout_args(fmt_args: core::fmt::Arguments) {
+    unsafe { _print_to_stdout_args(fmt_args) }
+}
+
