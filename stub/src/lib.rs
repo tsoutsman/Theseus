3a5a6c01fb94dd42286db4f8d18dffe9cd63d3e8
Klim Tsoutsman <klim@tsoutsman.com>
2022-07-27 12:06:56 +1000

    Implement basic POC
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

---
+++ stub/src/lib.rs
@@ -0,0 +1,6 @@
+#![no_std]
+
+#[export_name = "terminal_print::print_to_stdout_args::h2e6f0d1c9e7a336c"]
+#[no_mangle]
+#[inline(never)]
+pub fn print_to_stdout_args(fmt_args: core::fmt::Arguments) {}
