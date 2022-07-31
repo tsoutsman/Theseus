dcf83da27d621cdd239e96e5084bb1cebe3131c0
tsoutsman <tsoutsman@users.noreply.github.com>
2022-07-31 04:58:49 +0000

    Add Github Action to run select clippy lints (#574)
    * By default, the invocation of clippy allows all lints, and then the ones specified in `/lints.txt` are explicitly denied.
    
    * As #526 progresses and more lints are supported, we can uncomment them from `/lints.txt`, with the goal of eventually removing that file once all lints have been addressed.
    
    * Currently, this clippy action is triggered when PRs are opened, reopened, edited, or synchronized.
    
    * Also fix some overlooked lint violations in `applications/ping`. 9b90e6d7ba6a53618a9b7f0b45b997ebbbd3f13e

---
+++ doc/mod_mgmt/serde/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["SerializedCrate","A (de)serializable representation of a loaded crate that is `serde`-compatible."],["SerializedSection","A (de)serializable representation of a loaded section that is `serde`-compatible."]]};
