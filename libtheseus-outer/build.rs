3d97013a5a0a9b5f8d380411b0a2f026c76ed8da
Klim Tsoutsman <klim@tsoutsman.com>
2022-08-02 23:08:53 +1000

    Changes to libtheseus build script
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- libtheseus-outer/build.rs
+++ libtheseus-outer/build.rs
@@ -1,4 +1,7 @@
+//! Forgive me father for I have sinned.
+
 #![feature(exit_status_error)]
+#![allow(unreachable_code)]
 
 use std::{env, path::PathBuf, process::Command};
 
@@ -21,28 +24,34 @@ fn main() {
     libtheseus_src.push("src");
     println!("cargo:rerun-if-changed={}", libtheseus_src.display());
 
-    // Prevents multiple libtheseus rlib files with different hashes.
-    Command::new("cargo")
-        .arg("clean")
-        .current_dir(&libtheseus_dir)
-        .status()
-        .expect("couldn't get cargo clean exit status")
-        .exit_ok()
-        // TODO: Also capture stderr
-        .expect("cargo clean failed");
+    let cargo_bin = env::var("CARGO").unwrap();
 
     let mut target_spec = manifest_dir.clone();
     target_spec.pop();
     target_spec.push("cfg");
     target_spec.push("x86_64-theseus.json");
 
-    Command::new("cargo")
+    let cargo_encoded_rustflags = env::var("CARGO_ENCODED_RUSTFLAGS").unwrap();
+    let (cfg_bootstrap, new_cargo_encoded_rustflags) =
+        cargo_encoded_rustflags.split_once("\u{1f}").unwrap();
+    // println!("new flags: {new_cargo_encoded_rustflags}");
+    // assert_eq!(cfg_bootstrap, "--cfg=bootstrap");
+    
+    // std::thread::sleep(std::time::Duration::from_secs(3));
+    
+    // println!("libtheseus: {}", libtheseus_dir.display());
+
+    Command::new(cargo_bin)
         .arg("build")
         .arg("--release")
         .arg("--target")
         .arg(target_spec)
         .arg("-Z")
         .arg("build-std=core,alloc")
+        .env_remove("CARGO_ENCODED_RUSTFLAGS")
+        .env_remove("RUSTC_LINT_FLAGS")
+        .env("CARGO_ENCODED_RUSTFLAGS", new_cargo_encoded_rustflags)
+        .env("RUSTC_BOOTSTRAP", "1")
         .current_dir(&libtheseus_dir)
         .status()
         .expect("couldn't get cargo build exit status")
@@ -64,6 +73,6 @@ fn main() {
     host_library_dir.push("deps");
     println!("cargo:rustc-link-search={}", host_library_dir.display());
 
-    println!("target libraries: {}", target_library_dir.display());
-    println!("host libraries: {}", host_library_dir.display());
+    // println!("target libraries: {}", target_library_dir.display());
+    // println!("host libraries: {}", host_library_dir.display());
 }
