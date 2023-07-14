b638a5008ef8317280cd8893b34acec4bf3f1766
tsoutsman <tsoutsman@users.noreply.github.com>
2023-07-14 10:09:39 +0000

    Replace old `mutex_sleep` with new `sync_block` (#1007)
    * Remove the old `mutex_sleep` crate.
    
    * Implement `Debug` and `Default` for the new `sync_block`
      `Mutex`/`RwLock` types.
    
    * Implement a `std`-style API for `sync_block`, in which
      the `lock()` functions return a `Result`.
    
    * Update `backtrace` and `wasmtime` ports to use the
      new `sync_block` `Mutex`/`RwLock` types.
    
    * Update `tlibc` lockfile to remove old usage of `mutex_sleep`.
    
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com> 403d3e280e0d7e20854922fe771a5776a9d9a4b5

--- doc/implementors/fs_node/trait.FsNode.js
+++ doc/implementors/fs_node/trait.FsNode.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "fs_node":[],
-"heapfile":[["impl <a class=\"trait\" href=\"fs_node/trait.FsNode.html\" title=\"trait fs_node::FsNode\">FsNode</a> for <a class=\"struct\" href=\"heapfile/struct.HeapFile.html\" title=\"struct heapfile::HeapFile\">HeapFile</a>"]],
+"heapfile":[["impl FsNode for <a class=\"struct\" href=\"heapfile/struct.HeapFile.html\" title=\"struct heapfile::HeapFile\">HeapFile</a>"]],
 "memfs":[["impl FsNode for <a class=\"struct\" href=\"memfs/struct.MemFile.html\" title=\"struct memfs::MemFile\">MemFile</a>"]],
 "root":[["impl FsNode for <a class=\"struct\" href=\"root/struct.RootDirectory.html\" title=\"struct root::RootDirectory\">RootDirectory</a>"]],
 "task_fs":[["impl FsNode for <a class=\"struct\" href=\"task_fs/struct.MmiFile.html\" title=\"struct task_fs::MmiFile\">MmiFile</a>"],["impl FsNode for <a class=\"struct\" href=\"task_fs/struct.MmiDir.html\" title=\"struct task_fs::MmiDir\">MmiDir</a>"],["impl FsNode for <a class=\"struct\" href=\"task_fs/struct.TaskDir.html\" title=\"struct task_fs::TaskDir\">TaskDir</a>"],["impl FsNode for <a class=\"struct\" href=\"task_fs/struct.TaskFs.html\" title=\"struct task_fs::TaskFs\">TaskFs</a>"],["impl FsNode for <a class=\"struct\" href=\"task_fs/struct.TaskFile.html\" title=\"struct task_fs::TaskFile\">TaskFile</a>"]],
